bddf0cf4290d6916c0ba3882a52fa9c1a7104e70
steven-solomon <solomon.steven.m@gmail.com>
2015-05-31 14:18:32 -0400

    Add test for order of invocations

--- ConsumerProducerTests/src/ProducerTests.java
+++ ConsumerProducerTests/src/ProducerTests.java
@@ -1,8 +1,10 @@
 import static org.mockito.Mockito.*;
+
 import java.util.concurrent.Semaphore;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 
 public class ProducerTests {
 	
@@ -34,5 +36,14 @@ public class ProducerTests {
 	@Test
 	public void adds_element_to_buffer() throws Exception {
 		verify(mockSimpleBuffer).add(anyString());
+	}
+	
+	@Test
+	public void invoke_al() throws Exception {
+		// Sadly it's impossible to remove this duplication
+		InOrder inOrder = inOrder(mockEmptyCount, mockSimpleBuffer, mockFillCount);
+		inOrder.verify(mockEmptyCount).acquire();
+		inOrder.verify(mockSimpleBuffer).add(anyString());
+		inOrder.verify(mockFillCount).release();	
 	}
 }
