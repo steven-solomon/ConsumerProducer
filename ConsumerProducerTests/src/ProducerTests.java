e69244db3e47b4036d22c8dc4125f3a564ffaf37
steven-solomon <solomon.steven.m@gmail.com>
2015-05-31 22:35:31 -0400

    Dont release fillCount after thread has been stopped Refactoring

--- ConsumerProducerTests/src/ProducerTests.java
+++ ConsumerProducerTests/src/ProducerTests.java
@@ -9,77 +9,90 @@ import org.mockito.InOrder;
 public class ProducerTests {
 	
 	Producer producer;
-	Semaphore mockEmptyCount;
+	Semaphore spiedEmptyCount;
 	Semaphore mockFillCount;
-	Semaphore semaphore;
+	Semaphore emptyCount;
 	SimpleBuffer mockSimpleBuffer;
+	Thread producerThread;
 	
 	@Before
 	public void setUp() throws Exception {
-		semaphore = new Semaphore(1);
-		mockEmptyCount = spy(semaphore);
+		makeProducer();
+		
+		startThread();
+	}
+
+	private void startThread() {
+		producerThread = new Thread(producer);
+		producerThread.start();
+	}
+
+	private void makeProducer() {
+		emptyCount = new Semaphore(1);
+		spiedEmptyCount = spy(emptyCount);
 		mockFillCount = mock(Semaphore.class);
 		mockSimpleBuffer = mock(SimpleBuffer.class);
-		producer = new Producer(mockEmptyCount, mockFillCount, mockSimpleBuffer);
+		producer = new Producer(spiedEmptyCount, mockFillCount, mockSimpleBuffer);
 	}
 		
 	@Test
 	public void aquires_permit_for_an_empty_element() throws Exception {	
-		Thread thread = new Thread(producer);
-		thread.start();
-		semaphore.release();
-		thread.join();
-		producer.stopRunning();
+		emptyCount.release();
+		stopThread();
 		
-		verify(mockEmptyCount).acquire();
+		verify(spiedEmptyCount).acquire();
+	}
+	
+	private void stopThread() throws InterruptedException {
+		producerThread.join();
+		producer.stopRunning();
 	}
 	
 	@Test
 	public void releases_permit_for_a_filled_element() throws Exception {
-		Thread thread = new Thread(producer);
-		thread.start();
-		semaphore.release();
-		thread.join();
-		producer.stopRunning();
+		emptyCount.release();
+		stopThread();
 		
 		verify(mockFillCount).release();
-	}
+	}	
 	
 	@Test
 	public void adds_element_to_buffer() throws Exception {
-		Thread thread = new Thread(producer);
-		thread.start();
-		semaphore.release();
-		thread.join();
-		producer.stopRunning();
+		emptyCount.release();
+		stopThread();
 		
 		verify(mockSimpleBuffer).add(anyString());
 	}
 	
 	@Test
 	public void invokes_steps_in_order() throws Exception {
-		Thread thread = new Thread(producer);
-		thread.start();
-		semaphore.release();
-		thread.join();
-		producer.stopRunning();
+		emptyCount.release();
+		stopThread();
 		
 		// Sadly it's impossible to remove this duplication
-		InOrder inOrder = inOrder(mockEmptyCount, mockSimpleBuffer, mockFillCount);
-		inOrder.verify(mockEmptyCount).acquire();
+		InOrder inOrder = inOrder(spiedEmptyCount, mockSimpleBuffer, mockFillCount);
+		inOrder.verify(spiedEmptyCount).acquire();
 		inOrder.verify(mockSimpleBuffer).add(anyString());
 		inOrder.verify(mockFillCount).release();	
 	}
 	
 	@Test 
-	public void does_not_add_to_buffer_when_it_is_stopped() throws InterruptedException {
-		Thread thread = new Thread(producer);
-		thread.start();
-		producer.stopRunning();		
-		semaphore.release();
-		thread.join();
+	public void does_not_add_to_buffer_when_stopped() throws InterruptedException {
+		stopBeforeEmptyCountReleased();
 		
-		verify(mockSimpleBuffer, never()).add(anyString());
+		verify(mockSimpleBuffer, never()).add(anyString());	
+	}
+	
+	private void stopBeforeEmptyCountReleased() throws InterruptedException {
+		producer.stopRunning();		
+		emptyCount.release();
+		producerThread.join();
+	}
+	
+	@Test 
+	public void does_not_release_fill_count_when_stopped() throws InterruptedException {
+		stopBeforeEmptyCountReleased();
+		
 		verify(mockFillCount, never()).release();			
 	}
 }
